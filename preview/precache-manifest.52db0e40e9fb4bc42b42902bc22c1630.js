a3e815c2a3237a9663706d5d7524a18032d267a7
andreyzagoruy-dev <andreyzagoruy.dev@gmail.com>
2019-12-10 19:42:48 +0200

    Application build added to repo

---
+++ preview/precache-manifest.52db0e40e9fb4bc42b42902bc22c1630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7320a54a79c80fcc0594c1919077350",
+    "url": "./index.html"
+  },
+  {
+    "revision": "12c6e78571a9d7d4624b",
+    "url": "./static/css/main.42b14203.chunk.css"
+  },
+  {
+    "revision": "af44914d46637ae03a91",
+    "url": "./static/js/2.c1fcb711.chunk.js"
+  },
+  {
+    "revision": "12c6e78571a9d7d4624b",
+    "url": "./static/js/main.7e48967b.chunk.js"
+  },
+  {
+    "revision": "c38d1a4c66a9677346f2",
+    "url": "./static/js/runtime-main.3e845c60.js"
+  },
+  {
+    "revision": "edcc802abe685c592e584a8bd911bc97",
+    "url": "./static/media/mouse.edcc802a.svg"
+  }
+]);
