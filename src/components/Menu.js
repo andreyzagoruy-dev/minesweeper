9ae1904c1aef73e063351302574ab7c42402f138
andreyzagoruy-dev <andreyzagoruy.dev@gmail.com>
2019-12-12 12:35:06 +0200

    Menu options implemented as arrays and minor code style changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,15 +8,23 @@ class Menu extends React.Component {
       difficulty: 10,
       size: 8
     }
+    this.sizeOptions = [
+      { name: 'Tiny', value: 6 },
+      { name: 'Normal', value: 8 },
+      { name: 'Huge', value: 10 },
+    ];
+    this.difficultyOptions = [
+      { name: 'Easy', value: 5 },
+      { name: 'Moderate', value: 10 },
+      { name: 'Hard', value: 15 },
+    ];
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
+    const { name, value } = event.target;
 
     this.setState({
       [name]: value
@@ -25,30 +33,57 @@ class Menu extends React.Component {
 
   handleSubmit(event) {
     const { startGame } = this.props;
+    const { difficulty, size } = this.state;
     event.preventDefault();
 
-    startGame(this.state);
+    startGame({
+      difficulty,
+      size
+    });
   }
 
   render() {
+    const { difficultyOptions, sizeOptions, handleChange, handleSubmit } = this;
+    const { difficulty, size } = this.state;
+
     return (
       <div className="menu">
         <h1 className="game-title">The Minesweeper</h1>
-        <form className="form-config" onSubmit={this.handleSubmit}>
+        <form className="form-config" onSubmit={handleSubmit}>
           <label className="form-config__label">
             Size
-            <select className="form-config__select" name="size" value={this.state.size} onChange={this.handleChange}>
-              <option value="6">Tiny</option>
-              <option value="8">Normal</option>
-              <option value="10">Huge</option>
+            <select
+              className="form-config__select"
+              name="size"
+              value={size}
+              onChange={handleChange}
+              >
+                {sizeOptions.map((option, index) => (
+                  <option
+                    key={'size_option_' + index}
+                    value={option.value}
+                    >
+                      {option.name}
+                  </option>
+                ))}
             </select>
           </label>
           <label className="form-config__label">
             Difficulty
-            <select className="form-config__select" name="difficulty" value={this.state.difficulty} onChange={this.handleChange}>
-              <option value="5">Easy</option>
-              <option value="10">Moderate</option>
-              <option value="15">Hard</option>
+            <select
+              className="form-config__select"
+              name="difficulty"
+              value={difficulty}
+              onChange={handleChange}
+              >
+                {difficultyOptions.map((option, index) => (
+                  <option
+                    key={'difficulty_option_' + index}
+                    value={option.value}
+                    >
+                      {option.name}
+                  </option>
+                ))}
             </select>
           </label>
           <button className="button button--primary form-config__button">New Game</button>
